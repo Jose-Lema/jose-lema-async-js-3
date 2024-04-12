910abc341ff73f12888bc9221a7acae6b97dffca
Jose Lema <jose.lema.2310@gmail.com>
2024-04-12 00:11:21 -0700

    rewrote promise to have res and rej attributes

--- api.js
+++ api.js
@@ -1,10 +1,32 @@
-const output = document.querySelector("#output");
-const button = document.querySelector("#set-alarm");
+// const output = document.querySelector("#output");
+// const button = document.querySelector("#set-alarm");
 
-function setAlarm() {
-  setTimeout(() => {
-    output.textContent = "Wake up!";
-  }, 1000);
+// function setAlarm() {
+//   setTimeout(() => {
+//     output.textContent = "Wake up!";
+//   }, 1000);
+// }
+
+// button.addEventListener("click", setAlarm);
+
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    if (delay < 0) {
+      throw new Error("Alarm delay must not be negative");
+    }
+    setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
 }
 
-button.addEventListener("click", setAlarm);
+button.addEventListener("click", () => {
+  alarm(name.value, delay.value)
+    .then((message) => (output.textContent = message))
+    .catch((error) => (output.textContent = `Couldn't set alarm: ${error}`));
+});
